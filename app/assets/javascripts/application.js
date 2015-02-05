7a4ecc696db5d9fd8a7fa1cbf86a5886dfd8d715
Gabriele Cirulli <gabrycir@gmail.com>
2015-02-05 14:41:05 +0100

    Add a basic test script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,8 @@
 // This is the entry point of the application. Let's give this a try:
-alert("The application boots!");
+console.log("The application boots!");
+
+var test = require("./dependency");
+
+test("Hello!");
+
+document.write("This was added by the JavaScript!");
---
+++ app/assets/javascripts/dependency.js
@@ -0,0 +1,3 @@
+// A test dependency. Also uses 6to5
+
+module.exports = (arg) => console.log("Your argument is", arg);
